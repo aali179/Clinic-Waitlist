89c6d7612589fcb3e153d0bd09a81b690b3f82fe
usamatariq2 <utari062@uottawa.ca>
2019-11-18 17:56:11 -0500

    Fixed up some things

--- app/src/main/java/com/example/clinic_seg2105/addClinicScreen.java
+++ app/src/main/java/com/example/clinic_seg2105/addClinicScreen.java
@@ -16,24 +16,33 @@ import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class addClinicScreen extends AppCompatActivity {
 
     private EditText phoneNumber;
-    private EditText nameClinicTextView;
+    private EditText nameClinic;
     private EditText emailAddress;
-
+    private Spinner insurance;
+    private Spinner payment;
     private Button saveButton;
+    private DatabaseReference clinicDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_clinic_screen);
 
+        phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+        nameClinic = (EditText) findViewById(R.id.nameOfClinic);
+        emailAddress = (EditText) findViewById(R.id.emailAddress);
+        insurance = (Spinner) findViewById(R.id.insurance);
+        payment = (Spinner) findViewById(R.id.payment);
+        saveButton = (Button) findViewById(R.id.saveButton);
+
         String[] insuranceSpinner = new String[]{"Through Employer", "Personal Insurance", "Other"};
         String[] paymentSpinner = new String[]{"Credit Card", "Cash", "Cheque"};
-
-        Spinner i = (Spinner) findViewById(R.id.insurance);
-        Spinner p = (Spinner) findViewById(R.id.payment);
 
         ArrayAdapter<String> insuranceAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, insuranceSpinner);
         ArrayAdapter<String> paymentAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, paymentSpinner);
@@ -41,11 +50,19 @@ public class addClinicScreen extends AppCompatActivity {
         insuranceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         paymentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        i.setAdapter(insuranceAdapter);
-        p.setAdapter(paymentAdapter);
+        insurance.setAdapter(insuranceAdapter);
+        payment.setAdapter(paymentAdapter);
 
 
     }
+
+    public void saveButtonFunction(){
+        clinicDB = FirebaseDatabase.getInstance().getReference("Clinic");
+
+
+    }
+
+
 
     //@Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
