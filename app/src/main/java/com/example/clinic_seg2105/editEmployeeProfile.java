37c11a4b8a1cde37be3b636b90860471fc69995c
dshah092@uottawa.ca <devanshcshah@gmail.com>
2019-11-30 16:26:03 -0500

    employee and hours functions

---
+++ app/src/main/java/com/example/clinic_seg2105/editEmployeeProfile.java
@@ -0,0 +1,108 @@
+package com.example.hospital_clinic;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class editEmployeeProfile extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText clinicNameUpdate, clinicEmailUpdate, clinicNumberUpdate, clinicAddressUpdate, clinicPasswordUpdate;
+    private Spinner insuranceSpinnerUpdate, paymentSpinnerUpdate;
+    private Button updateEmployee;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    DatabaseReference mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_employee_profile);
+
+        initializer();
+        spinnerSetUp();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.updateEmployee:
+                updateEmployeeFunction();
+                break;
+        }
+
+    }
+
+    private void initializer(){
+        clinicNameUpdate = (EditText) findViewById(R.id.clinicNameUpdate);
+        clinicEmailUpdate = (EditText) findViewById(R.id.clinicEmailUpdate);
+        clinicNumberUpdate = (EditText) findViewById(R.id.clinicNumberUpdate);
+        clinicAddressUpdate = (EditText) findViewById(R.id.clinicAddressUpdate);
+        clinicPasswordUpdate = (EditText) findViewById(R.id.clinicPasswordUpdate);
+        updateEmployee = (Button) findViewById(R.id.updateEmployee);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        updateEmployee.setOnClickListener(this);
+    }
+
+
+    private void spinnerSetUp(){
+        insuranceSpinnerUpdate = (Spinner) findViewById(R.id.insuranceSpinnerUpdate);
+        paymentSpinnerUpdate = (Spinner) findViewById(R.id.paymentSpinnerUpdate);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.insurance_types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        insuranceSpinnerUpdate.setAdapter(adapter);
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.payment_methods, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        paymentSpinnerUpdate.setAdapter(adapter2);
+    }
+
+    private void updateEmployeeFunction(){
+
+        if (mUser == null){
+            finish();
+            startActivity(new Intent(this, createAccount.class));
+        } else {
+
+            String temp_name = clinicNameUpdate.getText().toString().trim();
+            String temp_email = clinicEmailUpdate.getText().toString().trim();
+            String temp_number = clinicNumberUpdate.getText().toString().trim();
+            String temp_address = clinicAddressUpdate.getText().toString().trim();
+            String temp_insurance = insuranceSpinnerUpdate.getSelectedItem().toString().trim();
+            String temp_payment = paymentSpinnerUpdate.getSelectedItem().toString().trim();
+            String temp_password = clinicPasswordUpdate.getText().toString().trim();
+
+            mDatabase.child("Employees").child(mUser.getUid()).child("address").setValue(temp_address);
+            mDatabase.child("Employees").child(mUser.getUid()).child("email").setValue(temp_email);
+            mDatabase.child("Employees").child(mUser.getUid()).child("insurance").setValue(temp_insurance);
+            mDatabase.child("Employees").child(mUser.getUid()).child("name").setValue(temp_name);
+            mDatabase.child("Employees").child(mUser.getUid()).child("number").setValue(temp_number);
+            mDatabase.child("Employees").child(mUser.getUid()).child("payment").setValue(temp_payment);
+
+        }
+
+    }
+
+
+}
--- app/src/main/java/com/example/clinic_seg2105/editServiceScreen.java
+++ app/src/main/java/com/example/clinic_seg2105/editServiceScreen.java
@@ -1,252 +1,111 @@
 package com.example.clinic_seg2105;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.List;
-import java.sql.PreparedStatement;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class editServiceScreen extends AppCompatActivity implements View.OnClickListener {
 
-public class editServiceScreen extends AppCompatActivity {
+    private Spinner editServiceSpinner;
+    private EditText editServiceNewName;
+    private Spinner newRoleEditServiceSpinner;
+    private Button editChangeConfirm;
+    private Button cancelEdit;
 
-    // Calling an instance of the database class that stores all the information
-    ServiceDBHelper myDb;
-
-    ServiceRepo repo = new ServiceRepo(this);
-
-
-    // Creating EditText fields for NewName
-    private EditText NewName;
-    private TextView NewNamePrompt;
-    private TextView EditPrompt;
-    private TextView RolePrompt;
-
-    // Create Button field for Update
-    private Button update;
-    private Button add;
-    private Button del;
-
-    public static String person_type;
-    public static String service_type;
-    public String name;
-
-
-    Spinner spinner2;
-    Spinner spinner3;
+    DatabaseReference ref;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_service);
+        setContentView(R.layout.activity_edit_service_screen);
 
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        ///////////////////////////////////////////////////////////// SPINNER 3 /////////////////////////////////////////
-        spinner3 = (Spinner) findViewById(R.id.spinner3);
-        // Using login_dropdown (check string.xml) to allow user to select from dropdown menu
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.login_dropdown, android.R.layout.simple_spinner_item);
+        ref = FirebaseDatabase.getInstance().getReference();
+
+        spinnerSetUp();
+
+        editServiceNewName = (EditText) findViewById(R.id.editServiceNewName);
+        editChangeConfirm = (Button) findViewById(R.id.editChangeConfirm);
+        cancelEdit = (Button) findViewById(R.id.cancelEdit);
+
+        editChangeConfirm.setOnClickListener(this);
+        cancelEdit.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.editChangeConfirm:
+                editService();
+                break;
+            case R.id.cancelEdit:
+                finish();
+                Intent intent = new Intent(getApplicationContext(), adminScreen.class);
+                startActivity(intent);
+        }
+
+    }
+
+    private void spinnerSetUp(){
+        newRoleEditServiceSpinner = (Spinner) findViewById(R.id.newRoleEditServiceSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.service_role, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner3.setAdapter(adapter);
-        //When user clicks the dropdown menu, either onItemSelected will activate or onNothingSelected (nothing happens)
-        spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        newRoleEditServiceSpinner.setAdapter(adapter);
+
+
+        editServiceSpinner = (Spinner) findViewById(R.id.editServiceSpinner);
+        ref.child("Services").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String text = parent.getItemAtPosition(position).toString();
-                person_type = text;
-                Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                final List<String> servicesList = new ArrayList<String>();
+
+                for (DataSnapshot servSnapshot: dataSnapshot.getChildren()){
+                    String servName = servSnapshot.child("name").getValue(String.class);
+                    servicesList.add(servName);
+                }
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(editServiceScreen.this, android.R.layout.simple_spinner_dropdown_item, servicesList);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                editServiceSpinner.setAdapter(adapter);
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        ///////////////////////////////////////////////////////////// SPINNER 2 /////////////////////////////////////////
-        spinner2 = (Spinner) findViewById(R.id.spinner2);
-        // Using login_dropdown (check string.xml) to allow user to select from dropdown menu
-        //ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.login_dropdown, android.R.layout.simple_spinner_item);
-        //adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //spinner2.setAdapter(adapter2);
-        //When user clicks the dropdown menu, either onItemSelected will activate or onNothingSelected (nothing happens)
-        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                //Service selected = (Service) parent.getItemAtPosition(position);
-                //mTVservice.setText(selected.getService());
-
-                String text = parent.getItemAtPosition(position).toString();
-                service_type = text;
-                Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-        ////////////////////////////////////////////////// INITIALIZATION ////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //just some initialization
-        NewName = findViewById(R.id.newNameChoice);
-        NewNamePrompt = findViewById(R.id.newName);
-        RolePrompt = findViewById(R.id.newRole);
-        EditPrompt = findViewById(R.id.editChoose);
-        update = (Button)findViewById(R.id.updateService);
-        add = (Button)findViewById(R.id.addServ);
-        del = (Button)findViewById(R.id.deleteServ);
-
-        // We pass crrAccTextWatcher (a function) to the addTextChangedListener method to Name, Username, Password and
-        // password-reenter
-        //NewName.addTextChangedListener(crrAccTextWatcher);
-        //populateDB();
-        loadService();
-
-        // add screen
-        if (adminScreen.screenChoice == 1) {
-            EditPrompt.setVisibility(View.INVISIBLE);
-            spinner2.setVisibility(View.INVISIBLE);
-            update.setVisibility(View.INVISIBLE);
-            del.setVisibility(View.INVISIBLE);
-        }
-
-        //edit screen
-        if (adminScreen.screenChoice == 2) {
-            add.setVisibility(View.INVISIBLE);
-            del.setVisibility(View.INVISIBLE);
-        }
-
-        //delete screen
-        if (adminScreen.screenChoice == 3) {
-            add.setVisibility(View.INVISIBLE);
-            update.setVisibility(View.INVISIBLE);
-            NewName.setVisibility(View.INVISIBLE);
-            NewNamePrompt.setVisibility(View.INVISIBLE);
-            RolePrompt.setVisibility(View.INVISIBLE);
-            spinner3.setVisibility(View.INVISIBLE);
-        }
-
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addService();
-            }
-        });
-
-        update.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateService();
-            }
-        });
-
-        del.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                deleteService();
-            }
-        });
-
-
-        // edits data to database
-        // EditData();
     }
 
-    // LOGIN TEXT WATCHER METHOD //
-    // private TextWatcher crrAccTextWatcher = new TextWatcher() {
+    private void editService(){
 
-    //}
+        String serviceToBeUpdated = editServiceSpinner.getSelectedItem().toString().trim();
 
-    public void addService() {
-        Intent intent = new Intent(this, adminScreen.class);
+        String newServiceName = editServiceNewName.getText().toString().trim();
+        String newServiceRole = newRoleEditServiceSpinner.getSelectedItem().toString().trim();
 
-        //deletes old service
-        //repo.delete(service_type);
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Services").child(serviceToBeUpdated);
 
-        //insert new service
-        Service service = new Service();
-        service.setRole(person_type);
-        service.setService(NewName.getText().toString());
-        repo.insert(service);
-
-        startActivity(intent);
+        ref.child("name").setValue(newServiceName);
+        ref.child("role").setValue(newServiceRole);
+        ref.setValue(newServiceName);
     }
 
-    public void updateService() {
-        Intent intent = new Intent(this, adminScreen.class);
-
-        //deletes old service
-        repo.delete(service_type);
-
-        //insert new service
-        Service service = new Service();
-        service.setRole(person_type);
-        service.setService(NewName.getText().toString());
-        repo.insert(service);
-
-        startActivity(intent);
-    }
-
-    public void deleteService() {
-        Intent intent = new Intent(this, adminScreen.class);
-
-        //deletes old service
-        repo.delete(service_type);
-
-        //insert new service
-        //Service service = new Service();
-        //service.setRole(person_type);
-        //service.setService(NewName.getText().toString());
-        //repo.insert(service);
-
-        startActivity(intent);
-    }
-
-    private void loadService(){
-        ArrayAdapter<String> spinnerAdapter;
-        ServiceRepo db = new ServiceRepo(getApplicationContext());
-        List<String> services = db.getAll();
-        spinnerAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, services);
-        spinner2.setAdapter(spinnerAdapter);
-
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-    }
-
-    private void populateDB(){
-
-        //ServiceRepo repo = new ServiceRepo(this);
-        //repo.Delete();
-
-        for (Integer i=0;i<5;i++){
-            Service service = new Service();
-            service.setRole("Patient");
-            service.setService("Checkup " + i.toString());
-
-            repo.insert(service);
-
-
-        }
-
-    }
 }
