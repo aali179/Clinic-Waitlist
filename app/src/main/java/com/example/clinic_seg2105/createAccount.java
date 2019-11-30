02ce1823057daf91fe928ba4e25901e29223da35
usamatariq2 <utari062@uottawa.ca>
2019-11-30 17:02:41 -0500

    idk

--- app/src/main/java/com/example/clinic_seg2105/createAccount.java
+++ app/src/main/java/com/example/clinic_seg2105/createAccount.java
@@ -1,30 +1,72 @@
 package com.example.clinic_seg2105;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class createAccount extends AppCompatActivity {
+public class createAccount extends AppCompatActivity implements View.OnClickListener {
 
-
-    public String user_name = MainActivity.user_name;
-    public String person_type = MainActivity.person_type;
+    private Button createNewEmployee, createNewPatient, signInAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
-        openWelcomeScreen();
+        createNewEmployee = (Button) findViewById(R.id.createNewEmployee);
+        createNewPatient = (Button) findViewById(R.id.createNewPatient);
+        signInAdmin = (Button) findViewById(R.id.signInAdmin);
+
+        createNewEmployee.setOnClickListener(this);
+        createNewPatient.setOnClickListener(this);
+        signInAdmin.setOnClickListener(this);
     }
 
-    //Displaying user information
-    public void openWelcomeScreen(){
-        TextView textView = (TextView) findViewById(R.id.welcomeScreen);
-        textView.append(" " + user_name + " you are logged in as " + person_type);
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.createNewEmployee:
+                createNewEmployeeFunction();
+                break;
+            case R.id.createNewPatient:
+                createNewPatientFunction();
+                break;
+            case R.id.signInAdmin:
+                signInAdminFunction();
+                break;
+        }
     }
 
+    private void createNewEmployeeFunction(){
+        finish();
+        Intent intent = new Intent(getApplicationContext(), employeeCreateAccount.class);
+        startActivity(intent);
+    }
+
+    private void createNewPatientFunction(){
+        finish();
+        Intent intent = new Intent(getApplicationContext(), patientCreateAccount.class);
+        startActivity(intent);
+
+    }
+
+    private void signInAdminFunction(){
+
+    }
 }
